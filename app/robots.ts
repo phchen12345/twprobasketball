d05f39c85a9fec5a3fb12e73f37122ce81298e81
Jasper Chen <tmsl9502@gmail.com>
2026-04-10 10:53:14 +0800

    Update sitemap and robots domain

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://twprobasketball.vercel.app/sitemap.xml",
+    sitemap: "https://taiwanprobasketball.vercel.app/sitemap.xml",
   };
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://twprobasketball.vercel.app",
+      url: "https://taiwanprobasketball.vercel.app",
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 1,
